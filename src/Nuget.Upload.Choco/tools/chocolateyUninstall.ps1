4029a48e47668fb2092bbea2bf79bfe11bae93f1
passuied <passuied@csod.com>
2016-12-30 01:01:15 -0800

    applying workaround provided in https://gist.github.com/ferventcoder/c6e446187d96a2436a09#file-chocolateyuninstall-ps1-L43-L46 for uninstall

--- src/Nuget.Upload.Choco/tools/chocolateyUninstall.ps1
+++ src/Nuget.Upload.Choco/tools/chocolateyUninstall.ps1
@@ -1,10 +1,9 @@
 $packageArgs = @{
   packageName = 'Cornerstone.NuGetUpload'
   fileType = 'msi'
-  file = '{53091262-B927-438C-A5B6-ABB8B8B0C0A3}'
-  silentArgs = '/quiet'
+  silentArgs = '{53091262-B927-438C-A5B6-ABB8B8B0C0A3} /quiet'
 }
 
-#Uninstall-ChocolateyPackage @packageArgs
-msiexec /x  '{53091262-B927-438C-A5B6-ABB8B8B0C0A3}' /quiet
+Uninstall-ChocolateyPackage @packageArgs
+#msiexec /x  '{53091262-B927-438C-A5B6-ABB8B8B0C0A3}' /quiet
 
